27ac28efdec41db408b53dd1145f442af18229b2
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-04-19 16:24:57 +0000

    HACKING

--- commandes/youtube.js
+++ commandes/youtube.js
@@ -1,4 +1,4 @@
-const { hack } = require("../framework/hack");
+const { zoukou } = require("../framework/hack");
 const yts = require('yt-search');
 const ytdl = require('ytdl-core');
 const fs = require('fs');
@@ -7,7 +7,7 @@ const ffmpeg = require("fluent-ffmpeg");
 const yts1 = require("youtube-yts");
 //var fs =require("fs-extra")
 
-hack({
+zoukou({
   nomCom: "song",
   categorie: "Search",
   reaction: "💿"
@@ -81,7 +81,7 @@ _*on downloading...*_\n\n`
 
   
 
-hack({
+zoukou({
   nomCom: "video",
   categorie: "Search",
   reaction: "🎥"
