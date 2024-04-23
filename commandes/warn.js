acffea1f2a0da37c0c605c6933a8d81160de7f05
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-04-23 00:04:26 +0000

    HACKING

--- commandes/warn.js
+++ commandes/warn.js
@@ -11,29 +11,29 @@ zokou(
     },async (dest,zk,commandeOptions) => {
 
  const {ms , arg, repondre,superUser,verifGroupe,verifAdmin , msgRepondu , auteurMsgRepondu} = commandeOptions;
-if(!verifGroupe ) {repondre('this is a group commands') ; return};
+if(!verifGroupe ) {repondre('Vous avez pas acces a cette commandes ,demande lui en priver') ; return};
 
 if(verifAdmin || superUser) {
-   if(!msgRepondu){repondre('reply a message of user to warn'); return};
+   if(!msgRepondu){repondre(*Mentionner la personne idiot faut savoir a avertir*); return};
    
    if (!arg || !arg[0] || arg.join('') === '') {
     await ajouterUtilisateurAvecWarnCount(auteurMsgRepondu)
    let warn = await getWarnCountByJID(auteurMsgRepondu)
    let warnlimit = s.WARN_COUNT
    
-   if( warn >= warnlimit ) { await repondre('this user reach limit of warning , so i kick him/her');
+   if( warn >= warnlimit ) { await repondre('Cet utilisateur a atteint le nombre maximum d\'avertissement , par consequent l'idiot  sera retirer ');
                 zk.groupParticipantsUpdate(dest, [auteurMsgRepondu], "remove")
  } else { 
 
     var rest = warnlimit - warn ;
-     repondre(`this user is warn , rest before kick : ${rest} `)
+     repondre(`Cet utilisateur idiot a un avertissement en plus dans sont casier faire attention; nombre d'avertissement restan tu sera  viré: ${rest} `)
    }
 } else if ( arg[0] === 'reset') { await resetWarnCountByJID(auteurMsgRepondu) 
 
-    repondre("Warn count is reset for this user")} else ( repondre('reply to a user by typing  .warn ou .warn reset'))
+    repondre("le nombre d'avertissement a été renitialiser pour cet utilisateur idiot faire attention")} else ( repondre('reply to a user by typing  .warn ou .warn reset'))
    
 }  else {
-    repondre('you are not admin')
+    repondre('Vous avez besoins des droits d\'adminitration monsieur soit logique')
 }
  
    });
