8d4e6d627963b9dfe30721daddd0f109a4bd0a16
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-05-06 15:16:30 +0000

    Update movie.js

--- commandes/movie.js
+++ commandes/movie.js
@@ -1,39 +1,43 @@
 const { zokou } = require("../framework/zokou");
-const yts = require('yt-search');
-const ytdl = require('ytdl-core');
-const fs = require('fs');
-const yt=require("../framework/dl/ytdl-core.js")
-const ffmpeg = require("fluent-ffmpeg");
-const yts1 = require("youtube-yts");
-//var fs =require("fs-extra")
+const { getJson } = require("../framework/utils");
 
-		zokou({
+zokou({
   nomCom: "movie",
   categorie: "Search",
   reaction: "💿"
 }, async (origineMessage, zk, commandeOptions) => {
-  const { ms, repondre, arg } = commandeOptions;
-     
+  const { repondre, arg } = commandeOptions;
+
   if (!arg[0]) {
-    repondre("quelle film  veux-tu.");
+    repondre("Quel film veux-tu ?");
     return;
   }
-	async (message, match) => {
-		const movie = await getJson(
-			`http://www.omdbapi.com/?apikey=742b2d09&t=${match}&plot=full`
-		)
-		if (movie.Response != 'True')
-			return await message.send('*Not found*', {
-				quoted: message.data,
-			})
-		let msg = ''
-		const url = movie.Poster
-		delete movie.Poster
-		delete movie.Response
-		delete movie.Ratings
-		for (const data in movie)
-			if (movie[data] != 'N/A') msg += `*${data} :* ${movie[data]}\n`
-		if (url == 'N/A') return await message.send(msg.trim())
-		return await message.sendFromUrl(url, { caption: msg.trim() })
-	}
-)
+
+  const movieTitle = arg.join(" ");
+  const movie = await getJson(
+    `http://www.omdbapi.com/?apikey=742b2d09&t=${encodeURIComponent(movieTitle)}&plot=full`
+  );
+
+  if (movie.Response !== 'True') {
+    repondre('*Not found*');
+    return;
+  }
+
+  let msg = '';
+  const url = movie.Poster;
+  delete movie.Poster;
+  delete movie.Response;
+  delete movie.Ratings;
+
+  for (const data in movie) {
+    if (movie[data] !== 'N/A') {
+      msg += `*${data} :* ${movie[data]}\n`;
+    }
+  }
+
+  if (url === 'N/A') {
+    repondre(msg.trim());
+  } else {
+    await zk.sendMessage(origineMessage.from, { url }, { caption: msg.trim() });
+  }
+});
