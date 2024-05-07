6c8c64038ba38bee34cb72d35521a7844640f110
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-05-07 19:11:27 +0000

    Update repos.js

--- commandes/repos.js
+++ commandes/repos.js
@@ -22,7 +22,7 @@ zokou({ nomCom: "repo", catégorie:"Général", reaction: "🌏", nomFichier: __
       const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
       const gitdata = `HELLO 👋 
-voici *Hacking-MD.*\n YOU DEPLOYE THIS *REPO*, *SESSION_ID*  'https://md-thomas-what34.000webhostapp.com/'
+THIS YOU *Hacking-MD.*\n YOU DEPLOYE THIS *REPO*, *SESSION_ID*  https://md-thomas-what34.000webhostapp.com/
 
 🗼 *REPOSITORY:* ${data.html_url}
 ✨ *STARS:* ${repoInfo.stars}
